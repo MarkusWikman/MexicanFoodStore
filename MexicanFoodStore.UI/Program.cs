1600b3d1081600e9148109075d4e2f08604ac34f
MarkusWikman <Wikman.markus@gmail.com>
2024-04-08 09:27:47 +0200

    Fixed program.cs in UI

--- MexicanFoodStore.UI/Program.cs
+++ MexicanFoodStore.UI/Program.cs
@@ -1,4 +1,9 @@
+using Blazored.LocalStorage;
+using MexicanFoodStore.API.DTO.DTOs;
 using MexicanFoodStore.UI;
+using MexicanFoodStore.UI.Http.Clients;
+using MexicanFoodStore.UI.Models.Link;
+using MexicanFoodStore.UI.Services;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 
@@ -7,6 +12,22 @@ builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services.AddSingleton<UIService>();
+builder.Services.AddBlazoredLocalStorageAsSingleton();
+builder.Services.AddSingleton<IStorageService, LocalStorage>();
+builder.Services.AddHttpClient<CategoryHttpClient>();
+builder.Services.AddHttpClient<ProductHttpClient>();
+ConfigureAutoMapper();
 
 
 await builder.Build().RunAsync();
+
+void ConfigureAutoMapper()
+{
+    var config = new MapperConfiguration(cfg =>
+    {
+        cfg.CreateMap<CategoryGetDTO, LinkOption>().ReverseMap();
+    });
+    var mapper = config.CreateMapper();
+    builder.Services.AddSingleton(mapper);
+}
