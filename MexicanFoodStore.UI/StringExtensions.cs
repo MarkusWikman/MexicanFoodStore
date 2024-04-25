638ea46d42234b949686ab7a5696b1f280a75a14
Markus Wikman <Wikman.markus@gmail.com>
2024-04-25 09:04:22 +0200

    Fixade string förkortningen

---
+++ MexicanFoodStore.UI/StringExtensions.cs
@@ -0,0 +1,11 @@
+﻿namespace MexicanFoodStore.UI
+{
+    public static class StringExtensions
+    {
+        public static string Truncate(this string value, int length)
+        {
+            if (value.Length < length) return value;
+            return value.Trim().Substring(length - 4).Trim() + "...";
+        }
+    }
+}
