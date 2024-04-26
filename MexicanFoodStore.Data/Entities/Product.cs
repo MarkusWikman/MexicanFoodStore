acee78821af934a4f790d9a10156bf951d0edd11
Markus Wikman <Wikman.markus@gmail.com>
2024-04-26 10:49:03 +0200

    Added property amount to product

--- MexicanFoodStore.Data/Entities/Product.cs
+++ MexicanFoodStore.Data/Entities/Product.cs
@@ -6,6 +6,7 @@
         public string? Name { get; set; }
         public string? Description { get; set; } = string.Empty;
         public string? PictureURL { get; set; } = string.Empty;
+        public int Amount { get; set; } = 1; // Customer will be able to choose more but its always automatically one
         public List<Category>? Categories { get; set; }
     }
 }
